91e4fd14e98e12c03e2795693ca151f25ec99261
beomq <beomdolq@gmail.com>
2023-06-17 01:18:56 +0900

    first_commit

---
+++ lib/stop_watch_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class StopWatchScreen extends StatefulWidget {
+  const StopWatchScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StopWatchScreen> createState() => _StopWatchScreenState();
+}
+
+class _StopWatchScreenState extends State<StopWatchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('스톱워치'),
+      ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 30,
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                '0',
+                style: TextStyle(fontSize: 50),
+              ),
+              Text(
+                '00',
+              ),
+            ],
+          ),
+          const Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FloatingActionButton(
+                backgroundColor: Colors.grey,
+                onPressed: () {},
+                child: const Icon(Icons.refresh),
+              ),
+              FloatingActionButton(
+                backgroundColor: Colors.brown,
+                onPressed: () {},
+                child: const Icon(Icons.play_arrow),
+              ),
+              FloatingActionButton(
+                backgroundColor: Colors.blueGrey,
+                onPressed: () {},
+                child: const Icon(Icons.add),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 30,
+          )
+        ],
+      ),
+    );
+  }
+}
